0064a1d07925e9e990a5d88529c62bf7efbea18c
MichelLTP <michel.pomerantzeff@LTPlabs.com>
2025-07-16 15:40:54 +0100

    xxx

---
+++ app/welcome/welcome.tsx
@@ -0,0 +1,13 @@
+import image from "../../public/sample.png";
+
+export function Welcome() {
+  return (
+    <div className="fixed inset-0 bg-gray-100">
+      <img
+        src={image}
+        alt="Centered"
+        className="w-full h-full object-center object-contain"
+      />
+    </div>
+  );
+}

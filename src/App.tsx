2054bd806376681935568ebf19c85b381908725c
MichelLTP <michel.pomerantzeff@LTPlabs.com>
2025-07-17 08:46:44 +0100

    test

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,21 @@ import image from "/sample.png";
 import "./App.css";
 
 function App() {
+  const handleTouchStart = (event: React.TouchEvent) => {
+    if (event.touches.length === 2) {
+      alert("Double touch detected!");
+      // You can trigger any action here
+    }
+  };
+
   return (
     <div className="welcome-container">
-      <img src={image} alt="Centered" className="welcome-image" />
+      <img
+        onTouchStart={handleTouchStart}
+        src={image}
+        alt="Centered"
+        className="welcome-image"
+      />
     </div>
   );
 }
